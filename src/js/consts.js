e54aad8f80918392646e7ef03047d1ad50c7ff35
Mansur <mansurtuhugov3@gmail.com>
2023-11-07 15:40:50 +0300

    Add hanngman games files

---
+++ src/js/consts.js
@@ -0,0 +1,3 @@
+export const WORDS = ["code", "frontend", "programming", "hacker", "computer", "laptop", "monitor", "progress", "innovator", "digitalization", "junior", "midle", "senior"]
+
+export const KEYBOARD_LETTERS = Array.from("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
